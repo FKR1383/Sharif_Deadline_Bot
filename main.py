d795db64521e27e36e1064ef26dda78c282fca62
Farzam Koohi <Farzamprogrammer@gmail.com>
2024-02-09 20:16:19 +0330

    first try - good work :)

--- main.py
+++ main.py
@@ -40,4 +40,27 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(response)
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+    print(f'Update {update} caused error {context.error}')
+
+
+if __name__ == "__main__":
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    #Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+
+    #Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    #Errors
+    app.add_error_handler(error)
+
+    #Polls the bot
+    print('Polling...')
+    app.run_polling(poll_interval=3)
+
+
+
