63d7f75f12a03decf7553d0f97764c4b4efe29ad
Farzam Koohi <Farzamprogrammer@gmail.com>
2024-02-10 01:59:33 +0330

    in progress ...

--- main.py
+++ main.py
@@ -3,18 +3,73 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from mysql.connector import connect, Error
 
-TOKEN: Final = '6898166331:AAE3SxcJjYu01NCsbvlZHs9Gzkcj7RQmM80'
+
 BOT_USERNAME: Final = '@Sharif_Deadline_bot'
+
+async def add_user_to_database(update: Update):
+    try:
+        connection = connect(host='localhost',
+                                             database='sharif_deadline_database',
+                                             user='admin',
+                                             password='admin00')
+        mySql_insert_query = f"""INSERT INTO users (user_id, username) 
+                               VALUES 
+                               ({update.message.chat.id}, '{update.message.chat.username}') """
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        print(cursor.rowcount, "User added!")
+        cursor.close()
+
+    except Error as error:
+        print("Failed to insert record into Laptop table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
+
+
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام. به ربات اعلام ددلاین شریف خوش آمدید :) در اسرع وقت درس هاتو بگو که بتونم کمکت کنم.")
+    await update.message.reply_text("سلام. به ربات اعلام ددلاین شریف خوش آمدید :) در اسرع وقت درس هاتو بگو که بتونم کمکت کنم. (لیست دروس رو برات گذاشتیم :) )")
+    await add_user_to_database(update)
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("این بات برای کمک به فراموش نکردن ددلاین ها ساخته شده.")
 
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Nothing!")
+async def get_all_courses_from_database():
+    try:
+        connection = connect(host='localhost',
+                                             database='sharif_deadline_database',
+                                             user='admin',
+                                             password='admin00')
+
+        sql_select_Query = "SELECT course_id, group_id, course_name, lecturers_name FROM course"
+        cursor = connection.cursor()
+        cursor.execute(sql_select_Query)
+        records = cursor.fetchall()
+        result = ''
+        for row in records:
+            result += ("کد درس = " + str(row[0])) + ' | '
+            result += ("کد گروه = " + str(row[1])) + ' | '
+            result += ("اسم درس = " + str(row[2])) + ' | '
+            result += ("اسم اساتید = " + str(row[3])) + ' | '
+            result += '\n'
+        return result
+
+    except Error as e:
+        print("Error reading data from MySQL table", e)
+    finally:
+        if connection.is_connected():
+            connection.close()
+            cursor.close()
+            print("MySQL connection is closed")
+
+async def all_courses_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    result = await get_all_courses_from_database()
+    await update.message.reply_text(result)
 
 # Handle Responses
 
@@ -37,7 +92,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             response: str = handle_response(new_text)
         else:
             response: str = handle_response(text)
-        print('Bot:', response)
+        print('Bot:'
+
+
+              , response)
         await update.message.reply_text(response)
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -50,7 +108,10 @@ def connect_to_database():
                 user="admin",
                 password="admin00",
         ) as connection:
-            print(connection)
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM Sharif_Deadline_Database.users")
+                for db in cursor:
+                    print(db)
     except Error as e:
         print(e)
 
@@ -64,7 +125,7 @@ if __name__ == "__main__":
     #Commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('custom', custom_command))
+    app.add_handler(CommandHandler('courses', all_courses_command))
 
     #Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
